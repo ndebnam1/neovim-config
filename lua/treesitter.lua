849e9f64f685671c1e81427713f5687d9e0b3b5f
ndebnam1 <ndebnam1@uncc.edu>
2023-02-09 15:10:55 -0500

    Inital Commit

---
+++ lua/treesitter.lua
@@ -0,0 +1,6 @@
+require'nvim-treesitter.configs'.setup {
+ensure_installed = "all", -- one of "all", "maintainted" (parsers with mainters), or a list of languages
+highlight = {
+	enable = true, -- false will disable the enire extension
+},
+}

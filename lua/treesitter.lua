ba08279d6850060e763b2c69a331fdfd0f80a048
ndebnam1 <ndebnam1@uncc.edu>
2023-02-13 21:41:05 -0500

    Revert "Inital Commit"
    This reverts commit 849e9f64f685671c1e81427713f5687d9e0b3b5f.

--- lua/treesitter.lua
+++
@@ -1,6 +0,0 @@
-require'nvim-treesitter.configs'.setup {
-ensure_installed = "all", -- one of "all", "maintainted" (parsers with mainters), or a list of languages
-highlight = {
-	enable = true, -- false will disable the enire extension
-},
-}
